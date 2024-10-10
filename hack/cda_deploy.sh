68c3a84ef154e6cd086a4e457fb7d20d9d4269db
Thomas Haller <thaller@redhat.com>
2024-10-10 21:02:54 +0200

    hack: detect the DPU kind for e2e host
    Currently, for e2e test we only have the config files
    "config-dpu-{,host}.yaml". Those currently only work with IPU systems.
    
    Next, we will add "config-marvell-dpu-{,host}.yaml". Those files are
    currently different than the ones for IPU. Hence, the "cda_deploy.sh"
    needs to detect which one to use.
    
    Add code to detect this based on the current host name.
    
    In the future, we may want to unify all DPU configuration files, and
    fill them with information from the google spread sheet. Note that
    currently does not work, because the files are different and the right
    information cannot be fetched from the spread sheet. But even if that is
    implemented, and cda-deploy fills in the information from the spread
    sheet, the detection is *also* based on the hostname. The only
    difference will be, that it's then all encoded in the spread sheet. This
    will happen later.
    
    For now, add code to detect the DPU kind and select the right config
    file.

--- hack/cda_deploy.sh
+++ hack/cda_deploy.sh
@@ -3,10 +3,38 @@
 set -e
 set -x
 
+die() {
+    printf "%s\n" "$*"
+    exit 1
+}
+
 cd "$(dirname "$0")/.."
 
-CLUSTER_CONFIG="$1"
-test -n "$CLUSTER_CONFIG"
+source ./hack/_source_common.sh
+
+CLUSTER_KIND="$1"
+
+DPU_KIND="$(detect_dpu_kind)"
+
+CLUSTER_CONFIG=
+case "$CLUSTER_KIND" in
+        "dpu")
+            case "$DPU_KIND" in
+                dpu)
+                    CLUSTER_CONFIG="config-dpu.yaml"
+                    ;;
+            esac
+            ;;
+        "dpu-host")
+            case "$DPU_KIND" in
+                dpu)
+                    CLUSTER_CONFIG="config-dpu-host.yaml"
+                    ;;
+            esac
+            ;;
+esac
+
+test -n "$CLUSTER_CONFIG" || die "Could not detect cluster config for CLUSTER_KIND=\"$CLUSTER_KIND\" and DPU_KIND=\"$DPU_KIND\""
 
 CLUSTER_CONFIG_PATH="hack/cluster-configs/$CLUSTER_CONFIG"
 test -f "$CLUSTER_CONFIG_PATH"
@@ -19,9 +47,22 @@ pushd cluster-deployment-automation
     python cda.py -v debug --secret /root/pull_secret.json "../$CLUSTER_CONFIG_PATH" deploy || ret=$?
 popd
 
-if [ "$ret" = 0 ]; then
-    echo "Successfully Deployed Cluster $CLUSTER_CONFIG"
-else
+if [ "$ret" != 0 ]; then
     echo "cluster-deployment-automation deployment of $CLUSTER_CONFIG failed with error code $ret"
+    exit "$ret"
 fi
-exit "$ret"
+
+case "$CLUSTER_KIND" in
+    "dpu")
+        KUBECONFIG="/root/kubeconfig.microshift"
+        ;;
+    "dpu-host")
+        KUBECONFIG="/root/kubeconfig.ocpcluster"
+        ;;
+esac
+export KUBECONFIG
+
+oc -n openshift-dpu-operator wait --for=condition=Ready pod --all --timeout=3m || :
+oc -n openshift-dpu-operator get all
+
+echo "Successfully Deployed Cluster $CLUSTER_CONFIG"
