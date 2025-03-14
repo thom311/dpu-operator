ecb3342d08eba41fb73ac1fe8dd8678ed2e1a826
Thomas Haller <thaller@redhat.com>
2025-03-14 06:12:25 -0400

    TMP: debug CDA

--- hack/both.sh
+++ hack/both.sh
@@ -9,10 +9,10 @@ source /tmp/cda-venv/bin/activate
 ./cda.py --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu-host.yaml deploy -f
 
 parallel -u --halt 2 ::: \
-  "./cda.py --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu.yaml deploy && echo 'Successfully Deployed ISO Cluster'" \
-  "./cda.py --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu-host.yaml deploy --steps pre,masters && echo 'Successfully Deployed DPU host Cluster'"
+  "./cda.py -v debug --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu.yaml deploy && echo 'Successfully Deployed ISO Cluster'" \
+  "./cda.py -v debug --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu-host.yaml deploy --steps pre,masters && echo 'Successfully Deployed DPU host Cluster'"
 
-./cda.py --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu-host.yaml deploy --steps workers,post && echo "Successfully Deployed DPU host Cluster"
+./cda.py -v debug --secret /root/pull_secret.json ../hack/cluster-configs/config-dpu-host.yaml deploy --steps workers,post && echo "Successfully Deployed DPU host Cluster"
 
 # WA to avoid starting the e2e-tests before CRDs are fully installed
 sleep 60
